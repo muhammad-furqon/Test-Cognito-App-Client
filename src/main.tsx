dcdfd2fd06b11463185562defaa0db4235979e58
muhammad-furqon <muhammad.furqon@cosmedia.co.jp>
2024-12-23 09:38:10 +0900

    Fix cognito auth config - Fix redirect-uri - Fix scope

--- src/main.tsx
+++ src/main.tsx
@@ -13,9 +13,10 @@ Amplify.configure(outputs);
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_KYJVPWPTn",
   client_id: "5klenrf64bb1nv9egl2t2ktb4g",
-  redirect_uri: "http://localhost:5173/",
+  redirect_uri: "https://main.d2d1d8kuit8n8u.amplifyapp.com/", //hard coded for now
   response_type: "code",
-  scope: "aws.cognito.signin.user.admin email openid phone profile",
+  scope: "email openid",
+  // scope: "aws.cognito.signin.user.admin email openid",
 };
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
