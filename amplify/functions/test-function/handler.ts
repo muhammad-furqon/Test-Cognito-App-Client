4e0802df140ed4e16ed33a6b00e2fa15f21ba089
muhammad-furqon <muhammad.furqon@cosmedia.co.jp>
2024-12-26 14:10:47 +0900

    Change from http to https

--- amplify/functions/test-function/handler.ts
+++ amplify/functions/test-function/handler.ts
@@ -40,7 +40,7 @@ export const handler: Handler = async (event, context): Promise<LambdaResult> =>
 
     try{
         console.log('options', options);
-        const response = await fetch('http://lambda-furl-d2d1d8kuit8n8u.auth.ap-northeast-1.amazoncognito.com/oauth2/token', options);  
+        const response = await fetch('https://lambda-furl-d2d1d8kuit8n8u.auth.ap-northeast-1.amazoncognito.com/oauth2/token', options);  
         console.log(response);
     }
     catch {
--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,7 @@ async function testFunction(code: string){
 
     try{
         console.log('options', options);
-        const response = await fetch('http://lambda-furl-d2d1d8kuit8n8u.auth.ap-northeast-1.amazoncognito.com/oauth2/token', options);  
+        const response = await fetch('https://lambda-furl-d2d1d8kuit8n8u.auth.ap-northeast-1.amazoncognito.com/oauth2/token', options);  
         console.log(response);
     }
     catch {
